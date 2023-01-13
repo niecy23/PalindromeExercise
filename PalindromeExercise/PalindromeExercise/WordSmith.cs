7ab0c8214735444ea61cf6c58d3320ba644d0978
Chanise Hart <niecy23@gmail.com>
2023-01-12 21:04:55 -0700

    Exercise 2 - Initial Commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		//Method to determine if a word is a palindrome.
+		public bool IsAPalindrome(string word)
+		{
+			char[] newArray = word.ToCharArray();
+			Array.Reverse(newArray);
+			var arrayReversed = new string(newArray);
+
+			if (arrayReversed.ToLower() == word.ToLower())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+	}
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,28 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("obstacle", false)]
+        [InlineData("tacocat", true)]
+        [InlineData("Madam", true)]
+        [InlineData("phone", false)]
+
+        public void Test1(string word, bool expected)
         {
+            //Arrange
+            var test = new WordSmith();
+
+            //Act
+            var actual = test.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
